c7270d2005272236987709b937996b42509dea57
Carlos Morales <camoralesl@correo.url.edu.gt>
2021-05-08 01:10:06 -0600

    Cambiando estructura del repo

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export const API_VERSION = "v1";
+// export const BASE_PATH = "http://localhost:4000/api";
+// export const BASE_PATH =
+//   "http://ec2co-ecsel-yoydf0x2rtva-915606723.us-east-1.elb.amazonaws.com:4000/api";
+
+export const BASE_PATH = "https://kingdom-gamer-server.herokuapp.com/api";
